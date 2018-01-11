0c067dd923247c83e9c84a086fb2ba880a4a4035
Ade Amos <ade@adeamos.com>
2018-01-11 21:53:55 +0000

    add css changes to make mobile responsive

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/public"));
 
 // Homepage
 app.get("/", function(req, res){
-    res.render("search");
+    res.render("search", {page:"home"});
 })
 
 // Search The Movie Database  using their API function
@@ -20,23 +20,23 @@ app.get("/results", function(req, res){
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
-            console.log(data);
-            res.render("results", {data: data});
+            res.render("results", {data: data, page: "results"});
         }    
     });
 });
 
+
 // Pulls information from my favorite movie list on The Movie Database
 app.get("/favorites", function(req, res){
-        var query = req.query.search;
+    var query = req.query.search;
     var apiKey = "ff0a38497fe407668201c3633ebc60f0";
     var flag = "&query="
     var url = "https://api.themoviedb.org/4/list/44714?page=1&api_key=" + apiKey;
+    var data = {};
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
-            var data = JSON.parse(body);
-            console.log(data);
-            res.render("favorites", {data: data});
+            data = JSON.parse(body);
+            res.render("favorites", {data: data, page: "favorites"});
         }    
     });
 });
